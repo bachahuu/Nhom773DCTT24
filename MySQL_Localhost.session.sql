bb6a0c8ba1606d0302bcc937f4adda49c21ca9cd
bachahuu <168210784+bachahuu@users.noreply.github.com>
2025-03-28 12:55:45 +0700

    CONNECT VSCODE -MYSOL(XAMPP)

---
+++ MySQL_Localhost.session.sql
@@ -0,0 +1,6 @@
+
+SELECT * FROM nhansu;
+INSERT INTO nguoidung (TenDangNhap, MatKhau, Email, HoTen, SoDienThoai, DiaChi, PhanQuyen, TrangThaiTaiKhoan, NgayTao, LanDangNhapCuoi, MaNhanSu)
+VALUES ('admin123', '123456', 'admin@example.com', 'Nguyen Van A', '0123456789', '123 Đường ABC', 'Admin', 'Hoạt động', '2024-03-28', NOW(), 2);
+
+SELECT * FROM nguoidung;
